dfaa7804e54ef7e4713aab7fd835b2af3345ae84
Charles François <charfrancois@auchan.fr>
2022-01-05 14:55:33 +0100

    :sparkles: initial commit

--- backend/config/admin.js
+++ backend/config/admin.js
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'e89f1940325b0b6314e31940896a56d5'),
+    secret: env('ADMIN_JWT_SECRET', 'fa2d5dd1fc2fc5eec37cc5f74a834e40'),
   },
 });
