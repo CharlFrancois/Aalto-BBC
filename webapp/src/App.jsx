75febc9dff9e417c650216e22bd3389806e5c64e
Drachebrume <baptiste.poulain59@gmail.com>
2022-01-07 16:29:31 +0100

    sidelines layout

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SideLines from './base/SideLines';
 import Navbar from './base/Navbar';
 import ButtonContact from './base/ButtonContact';
 
 function App() {
   return (
-    <div className="bg-skin-10">
+    <div className="bg-skin-10 h-full">
+      <SideLines />
       <Navbar />
       <ButtonContact />
+      <div className="mx-10">
+        hello
+      </div>
     </div>
   );
 }
--- webapp/src/base/Navbar.jsx
+++ webapp/src/base/Navbar.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default function Navbar() {
   return (
-    <div className="flex justify-between bg-skin-10 p-4 divide-x divide-gray-200">
-      <img src="Logo_AALTO.svg" alt="logo" />
-      <div className="flex divide-x divide-gray-200">
-        <img src="DarkMode.svg" className="p-2" alt="darkmode" />
-        <img src="Menu.svg" className="p-2" alt="menu-icon" />
+    <div className="mx-10 flex justify-between bg-skin-10 divide-x divide-skin-20 border-b border-skin-20">
+      <img src="Logo_AALTO.svg" alt="logo" className="p-7" />
+      <div className="flex divide-x divide-skin-20">
+        <img src="DarkMode.svg" className="p-2 px-8" alt="darkmode" />
+        <img src="Menu.svg" className="p-2 px-8" alt="menu-icon" />
       </div>
     </div>
   );
