d1558db3348f2da4173c7a4890bde47ed5d45ac1
Drachebrume <baptiste.poulain59@gmail.com>
2022-01-19 16:24:34 +0100

    navbar, modal, router

--- webapp/src/base/ButtonContact.jsx
+++ webapp/src/base/ButtonContact.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ButtonContact() {
   return (
     <div className="absolute bottom-5 right-5">
-      <button type="button" className="bg-dark p-4 rounded-full">
+      <button type="button" className="bg-custom-dark p-4 rounded-full">
         <img src="Arrow.svg" alt="arrow" />
       </button>
     </div>
