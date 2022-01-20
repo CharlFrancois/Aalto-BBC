bdfc20cee251ae60d68bd43e89be91b45027e6bd
Drachebrume <baptiste.poulain59@gmail.com>
2022-01-20 18:37:02 +0100

    project page

--- webapp/src/App.jsx
+++ webapp/src/App.jsx
@@ -4,20 +4,25 @@ import './App.css';
 import SideLines from './base/SideLines';
 import Navbar from './base/Navbar';
 import HomePage from './views/HomePage';
+import Projects from './views/Projects';
+import Project from './views/Project'
 
 function App() {
   return (
     <div className="bg-skin-10 h-full p-0 static overflow-x-hidden">
       <SideLines />
       <Navbar />
+      <div className="mt-[102px] w-full">
       <Routes>
-        <Route path="/" element={<span>Home</span>} />
-        <Route path="/projects" element={<span>Projets</span>} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/projects" element={<Projects />} />
         <Route path="/about" element={<span>A propos</span>} />
         <Route path="/journal" element={<span>Journal</span>} />
+        <Route path="/project/" element={<Project />} />
       </Routes>
       <img src='button-contact.png' className='fixed bottom-5 right-14 cursor-pointer' />
-      <HomePage />
+      </div>
+     
     </div>
   );
 }
---
+++ webapp/src/views/Project.jsx
@@ -0,0 +1,20 @@
+import { React, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+function Project() {
+    return (
+        <div className="mx-10 flex flex-col">
+          <div className="flex flex-row justify-between mt-14 mx-10">
+            <div className="text-[80px] font-gallery">
+              Nom projet
+            </div>
+            <div className="w-1/5 mt-6">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+              sed do eiusmod tempor incididunt ut labore et dolore magna ali. 
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default Project
